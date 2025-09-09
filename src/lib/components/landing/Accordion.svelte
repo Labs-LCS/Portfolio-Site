bcf07b3f9a7385ef37a809d4d06f9845f6202430
Lucas Campanhã dos Santos <lucas.camp@live.com>
2025-09-09 20:42:55 -0300

    Improve responsive image display on small screens.

--- src/lib/components/landing/Accordion.svelte
+++ src/lib/components/landing/Accordion.svelte
@@ -54,43 +54,47 @@
 				>...and about the duck</span
 			>
 		</summary>
-		<div class="w-full flex items-center justify-center">
-			<div class="text-justify p-3 flex flex-col md:max-w-[80%] gap-y-3 text-lg">
-				<span>Well, the story behind the duck is quite interesting!</span><span
-					>I'm a very hands-on person. Ever since I was a kid, my curiosity led me to take my toys
-					apart, which later evolved into a simple philosophy: if I can fix something, I will. To
-					this day, my friends still call me to borrow tools, assemble furniture, or change a shower
-					head—a real handyman. This philosophy also grew into a desire to build my own things, and
-					nothing enables that more than the world of programming.</span
-				>
+		<div class="text-justify p-3 flex flex-col gap-y-3 text-lg">
+			<div class="flex flex-col text-white gap-x-4 items-center gap-y-2">
+				<div class="flex md:max-w-[80%] px-2">
+					<div>
+						<span>Well, the story behind the duck is quite interesting!</span><span
+							>I'm a very hands-on person. Ever since I was a kid, my curiosity led me to take my
+							toys apart, which later evolved into a simple philosophy: if I can fix something, I
+							will. To this day, my friends still call me to borrow tools, assemble furniture, or
+							change a shower head—a real handyman. This philosophy also grew into a desire to build
+							my own things, and nothing enables that more than the world of programming.</span
+						>
 
-				<span
-					>This is where the duck comes into the story, and it's no coincidence. On a trip to Paris,
-					I came across a shop that only sold rubber ducks, which reminded me of the famous "rubber
-					duck debugging" method. To my delight, I found an item that perfectly merged my passion
-					for programming with my life philosophy: the DIY Duck from Budduck (an amazing store, by
-					the way!).</span
-				>
-				<div class="flex flex-col gap-y-1 text-center items-center justify-center">
-					<img
-						src={we}
-						alt="Me and my beloved girlfriend"
-						class="border border-orange-600 inset-shadow-2xs max-w-[400px]"
-					/>
-					<span class="text-sm text-gray-300 italic"
-						>That's me and my beloved girlfriend, who loves to cook!</span
-					>
+						<span
+							>This is where the duck comes into the story, and it's no coincidence. On a trip to
+							Paris, I came across a shop that only sold rubber ducks, which reminded me of the
+							famous "rubber duck debugging" method. To my delight, I found an item that perfectly
+							merged my passion for programming with my life philosophy: the DIY Duck from Budduck
+							(an amazing store, by the way!).</span
+						>
+						<div class="flex flex-col gap-y-1 text-center items-center justify-center">
+							<img
+								src={we}
+								alt="Me and my beloved girlfriend"
+								class="border border-orange-600 inset-shadow-2xs max-sm:max-w-[70dvw] py-3"
+							/>
+							<span class="text-sm text-gray-300 italic"
+								>That's me and my beloved girlfriend, who loves to cook!</span
+							>
+						</div>
+						<span
+							>The little duck embodied everything I stand for, and since then, I've decided to
+							adopt it as a symbol for my personal and open-source projects.</span
+						>
+
+						<span class="text-xs text-gray-300 italic"
+							>It's worth noting: all rights to the Deluxe DIY Duck belong to Budduck, which is why
+							I decided to design my own logo inspired by it, rather than using the official images
+							or products.</span
+						>
+					</div>
 				</div>
-				<span
-					>The little duck embodied everything I stand for, and since then, I've decided to adopt it
-					as a symbol for my personal and open-source projects.</span
-				>
-
-				<span class="text-xs text-gray-300 italic"
-					>It's worth noting: all rights to the Deluxe DIY Duck belong to Budduck, which is why I
-					decided to design my own logo inspired by it, rather than using the official images or
-					products.</span
-				>
 			</div>
 		</div>
 	</details>
